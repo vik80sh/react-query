f5d867d6d682babfeed2b7a2eb33752f0ead4bb5
Vikash Kumar Gupta <vik80sh@gmail.com>
2024-04-06 18:21:28 +0000

    updated

--- src/Variables/GlobalState.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "jotai";
-
-export const selectedComicsList = atom<{title: string; id: number}[]>([])
-export const userInput = atom<string>("")
-export const currentPageNumber = atom<number>(0)
-export const totalPagesAvailable = atom<number>(150)
-export const toggleButton = atom<boolean>(true)
